c6263407cd2889b7bde446b0ed4435a473081711
Miriam Nieslona <miriamnieslona@gmail.com>
2024-12-07 14:57:36 +0100

    Popróbowłam różne metody na sprawdzenie odstajacych i brakującyhc danych i różne wykresiki

---
+++ test_braki_odstajace_MN.R
@@ -0,0 +1,76 @@
+wynajem_pdst <- read.csv("apartments_rent_pl_2024_06.csv")
+
+#Instalowanie wymaganych pakietów do wizualizacji braków
+install.packages(c("tidyverse", "dlookr", "editrules", "VIM", "deducorrect", "ISLR","naniar","visdat"))
+
+#Ładowanie instalowanych pakietów
+library(tidyverse)
+library(dlookr)
+library(editrules)
+library(VIM)
+library(deducorrect)
+library(ISLR) 
+library(naniar)
+library(visdat)
+library(ggplot2)
+library(mice)
+library(finalfit)
+
+
+
+
+#Ile wierszy jest kompletnych (5778)
+sum(complete.cases(wynajem_pdst))
+#Ile % wierszy jest kompletnych (ok 65%)
+nrow(wynajem_pdst[complete.cases(wynajem_pdst), ])/nrow(wynajem_pdst)*100 
+
+is.special <- function(x){
+  if (is.numeric(x)) !is.finite(x) else is.na(x)
+}
+
+sapply(wynajem_pdst, is.special)
+
+for (n in colnames(wynajem_pdst)){
+  is.na(wynajem_pdst[[n]]) <- is.special(wynajem_pdst[[n]])
+}
+summary(wynajem_pdst)
+#Najwiecej NA w buildYeat (2249) i floor (1053)
+
+#Ile mamy braków danych (3602)
+n_miss(wynajem_pdst)
+
+#Ile % komorek to NA (1,45%)
+prop_miss(wynajem_pdst)
+
+#Zamieniamy puste wartosći na "NA"
+wynajem_pdst[wynajem_pdst == ''] <- NA
+print(wynajem_pdst)
+
+#Teraz mamy 15762 brakow danych
+n_miss(wynajem_pdst)
+
+#Czyli ok 6,36%
+prop_miss(wynajem_pdst)
+
+#Zwraca ile brakow danych dla konkretnych zmeinnych
+miss_var_summary(wynajem_pdst)
+# Condicion 72% brakow danych - ale mamy tylko "premium" - mozemy zalozyc ze te braki to 
+# "not premium" i zmaienic na binarne 0,1 
+
+#Wedlug wieszy - ile obserwacji w kazdym z wierszy jest brakujacych (np. tylko w 1 wierszu jest 8 brakujacyhc obserwacji)
+wynajem_pdst %>% 
+  miss_case_table()
+
+#Wizualizacje
+vis_miss(wynajem_pdst)
+vis_miss(wynajem_pdst, sort = TRUE) #braki posortowane
+gg_miss_fct(wynajem_pdst, fct = city) #braki w zaleznosci od miasta
+gg_miss_upset(wynajem_pdst, 
+              nsets = 10) #wspolwystepowanie NA
+gg_miss_upset(wynajem_pdst)
+vis_dat(wynajem_pdst)
+
+
+## zrobic jeszcez ODSTAJACE OBSERWACJE!!
+## jeszcze jakies zaleznosci miedzy tymi zmiennymi mamy poszukac?
+
